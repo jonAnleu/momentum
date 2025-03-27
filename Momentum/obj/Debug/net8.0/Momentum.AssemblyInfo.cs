591ff7757cf8a527f22c19515398af88b91a2136
Jonathan Anleu <jonathan.anleu@gmail.com>
2025-03-27 16:05:56 -0500

    icons added to Navbar links

--- Momentum/obj/Debug/net8.0/Momentum.AssemblyInfo.cs
+++ Momentum/obj/Debug/net8.0/Momentum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9af90ec4c5377ab4fbebdc41b74ed24bc86d453b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03590714864d3312a1571b8a961186a9b95a21c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
