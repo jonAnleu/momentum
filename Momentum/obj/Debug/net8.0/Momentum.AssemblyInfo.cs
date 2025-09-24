34db730eefb4b6f682d1f22a813ce22df43a56c4
Jon Anleu <96894253+jonAnleu@users.noreply.github.com>
2025-09-24 10:45:37 -0500

    testing component

--- Momentum/obj/Debug/net8.0/Momentum.AssemblyInfo.cs
+++ Momentum/obj/Debug/net8.0/Momentum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc24fb3c97e89000661dfcdba57d9f351f3a8728")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be7b48c81d68cacebb6ab6bbb39f30aa3dcaf9d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
