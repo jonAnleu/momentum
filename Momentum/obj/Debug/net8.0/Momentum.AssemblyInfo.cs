a141c2d8414ce1ab265209920744c69e41220fa0
Jon Anleu <96894253+jonAnleu@users.noreply.github.com>
2025-10-29 21:18:48 -0500

    Result chip design commit

--- Momentum/obj/Debug/net8.0/Momentum.AssemblyInfo.cs
+++ Momentum/obj/Debug/net8.0/Momentum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af1f6b06c68b4303bb34176c969b70ccc77ec49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c93366b780448b0497208aa15e4f4c9fa60471")]
 [assembly: System.Reflection.AssemblyProductAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
