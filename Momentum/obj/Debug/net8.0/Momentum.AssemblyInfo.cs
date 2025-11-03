3a634382ea3824572b181a50d32752d1e8d2b2c5
Jon Anleu <96894253+jonAnleu@users.noreply.github.com>
2025-11-03 14:58:46 -0600

    panels and fields commit

--- Momentum/obj/Debug/net8.0/Momentum.AssemblyInfo.cs
+++ Momentum/obj/Debug/net8.0/Momentum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e54b58c4a72ea43ef87a08ce0a5c54f43ad272e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f48c8e9f9a301badd5e774a61c283a07d146ba4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Momentum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
